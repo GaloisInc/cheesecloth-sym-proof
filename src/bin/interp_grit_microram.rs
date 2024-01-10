e20c9ad6bfc362512569d009892985397350ce1e
Stuart Pernsteiner <spernsteiner@galois.com>
2024-01-09 16:46:20 -0800

    progress toward proper microram build for interp_grit_microram

--- src/bin/interp_grit_microram.rs
+++ src/bin/interp_grit_microram.rs
@@ -10,24 +10,20 @@
 // eventually, but checking all `Result`s lets us catch problems sooner.
 #![deny(unused_must_use)]
 #![cfg_attr(feature = "deny_warnings", deny(warnings))]
-use std::cell::RefCell;
-use std::collections::BTreeMap;
-use std::env;
-use std::fs::File;
 use env_logger;
-use log::trace;
-use serde_cbor;
-use sym_proof::{Addr, Word};
+use sym_proof::Word;
 use sym_proof::advice;
 use sym_proof::interp;
 use sym_proof::kernel::Proof;
-use sym_proof::logic::{Term, Prop, Binder, VarCounter, ReachableProp, StatePred};
-use sym_proof::micro_ram::{self, Program, NUM_REGS};
+use sym_proof::logic::{TermKind, Prop, Binder, ReachableProp, StatePred};
+use sym_proof::micro_ram::{Program, NUM_REGS};
 use sym_proof::symbolic::{self, MemState, MemSnapshot};
 
 #[path = "../../gen/grit_program.rs"] mod program;
 #[path = "../../gen/grit_term_table.rs"] mod term_table;
 
+
+// Initial snapshot
 
 struct CpuState {
     pc: Word,
@@ -41,47 +37,147 @@ static mut SNAPSHOT_CPU_STATE: CpuState = CpuState {
     regs: [0; NUM_REGS],
 };
 
-thread_local! {
-    static SNAPSHOT_MEM: RefCell<BTreeMap<Addr, Word>> = RefCell::new(BTreeMap::new());
-}
 
-unsafe fn load_concrete_state() {
-    // Load the concrete state from disk so we don't need to rerun the concrete prefix.
-    #[cfg(not(feature = "playback_concrete_state"))] {
-        compile_error!("can't run proof interpreter without playback_concrete_state");
+#[cfg(not(feature = "microram"))]
+mod emulate_snapshot {
+    use std::cell::RefCell;
+    use std::collections::BTreeMap;
+    use sym_proof::{Addr, Word};
+    use sym_proof::micro_ram;
+    use super::{CpuState, SNAPSHOT_CPU_STATE};
+
+    thread_local! {
+        static SNAPSHOT_MEM: RefCell<BTreeMap<Addr, Word>> = RefCell::new(BTreeMap::new());
     }
-    #[cfg(feature = "playback_concrete_state")]
-    let conc_state: micro_ram::State = {
-        use std::fs::File;
-        let mut f = File::open("advice/concrete_state.cbor").unwrap();
-        serde_cbor::from_reader(f).unwrap()
-    };
 
-    unsafe {
-        SNAPSHOT_CPU_STATE = CpuState {
-            pc: conc_state.pc,
-            cycle: conc_state.cycle,
-            regs: conc_state.regs,
+    #[no_mangle]
+    extern "C" fn cc_load_snapshot_word(addr: u64) -> u64 {
+        SNAPSHOT_MEM.with(|rc| {
+            rc.borrow().get(&addr).copied().unwrap()
+        })
+    }
+
+    #[cfg(not(feature = "microram"))]
+    pub unsafe fn load_concrete_state() {
+        // Load the concrete state from disk so we don't need to rerun the concrete prefix.
+        #[cfg(not(feature = "playback_concrete_state"))] {
+            compile_error!("can't run proof interpreter without playback_concrete_state");
+        }
+        #[cfg(feature = "playback_concrete_state")]
+        let conc_state: micro_ram::State = {
+            use std::fs::File;
+            let f = File::open("advice/concrete_state.cbor").unwrap();
+            serde_cbor::from_reader(f).unwrap()
         };
-    }
 
-    SNAPSHOT_MEM.with(|rc| {
-        *rc.borrow_mut() = conc_state.mem;
-    });
+        unsafe {
+            SNAPSHOT_CPU_STATE = CpuState {
+                pc: conc_state.pc,
+                cycle: conc_state.cycle,
+                regs: conc_state.regs,
+            };
+        }
+
+        SNAPSHOT_MEM.with(|rc| {
+            *rc.borrow_mut() = conc_state.mem;
+        });
+    }
 }
 
+#[cfg(feature = "microram")]
 #[no_mangle]
 extern "C" fn cc_load_snapshot_word(addr: u64) -> u64 {
-    SNAPSHOT_MEM.with(|rc| {
-        rc.borrow().get(&addr).copied().unwrap()
-    })
+    assert!(addr % mem::size_of::<u64>() == 0);
+    ptr::read_volatile(addr as usize as *mut u64)
 }
 
 
-fn run() -> Result<(), String> {
+#[cfg(not(feature = "microram"))]
+mod emulate_advice {
+    use std::cell::RefCell;
+    use std::fs::File;
+    use serde_cbor;
+    use sym_proof::advice;
+
+    #[derive(Default)]
+    struct AdviceStream {
+        data: Vec<advice::Value>,
+        pos: usize,
+    }
+
+    impl AdviceStream {
+        pub fn new(data: Vec<advice::Value>) -> AdviceStream {
+            AdviceStream { data, pos: 0 }
+        }
+
+        pub fn next(&mut self) -> advice::Value {
+            assert!(self.pos < self.data.len(),
+                "tried to read past end of advice stream of length {}", self.data.len());
+            let x = self.data[self.pos];
+            self.pos += 1;
+            x
+        }
+    }
+
+    thread_local! {
+        static ADVICE: RefCell<AdviceStream> = RefCell::new(AdviceStream::default());
+    }
+
+    pub unsafe fn load_advice() {
+        // Load advice
+        let f = File::open("advice/linear.cbor").unwrap();
+        let advice: Vec<_> = serde_cbor::from_reader(f).unwrap();
+        ADVICE.with(|c| {
+            *c.borrow_mut() = AdviceStream::new(advice);
+        });
+    }
+
+    #[no_mangle]
+    extern "C" fn __cc_advise(max: advice::Value) -> advice::Value {
+        let x = ADVICE.with(|c| c.borrow_mut().next());
+        assert!(x <= max);
+        x
+    }
+}
+
+
+#[cfg(not(feature = "microram"))]
+fn exit() -> ! {
+    println!("ok");
+    std::process::exit(0);
+}
+
+#[cfg(feature = "microram")]
+fn exit() -> ! {
+    extern "C" {
+        fn __cc_answer(val: usize) -> !;
+    }
+    unsafe {
+        ptr::write_volatile(0xffff_ffff_ffff_fff0 as *mut usize, 1);
+        __cc_answer(1);
+    }
+}
+
+
+#[cfg(not(feature = "microram"))]
+#[track_caller]
+fn fail() -> ! {
+    panic!("fail")
+}
+
+#[cfg(feature = "microram")]
+fn fail() -> ! {
+    extern "C" {
+        fn __cc_answer(val: usize) -> !;
+    }
+    unsafe {
+        __cc_answer(0);
+    }
+}
+
+
+fn run() -> ! {
     let prog = Program::new(&program::PROG_INSTRS, &program::PROG_CHUNKS);
-    eprintln!("loaded code: {} instrs", prog.len());
-    //eprintln!("loaded memory: {} words", init_state.mem.len());
 
 
     // ----------------------------------------
@@ -114,57 +210,36 @@ fn run() -> Result<(), String> {
 
     interp::playback_proof(&mut pf, advice::playback::rules::Tag);
 
-    println!("\nfinal theorem:\n{}", pf.print(pf.props().last().unwrap()));
+    #[cfg(not(feature = "microram"))] {
+        println!("\nfinal theorem:\n{}", pf.print(pf.props().last().unwrap()));
+    }
 
-    println!("ok");
+    let prop = pf.props().last().unwrap();
+    let rp = match *prop {
+        Prop::Reachable(ref rp) => rp,
+        _ => fail(),
+    };
+    let min_cycles = match rp.min_cycles.kind() {
+        TermKind::Const(x) => x,
+        _ => fail(),
+    };
+    let ok = min_cycles >= 1000;
+    if !ok {
+        fail();
+    }
 
-    Ok(())
+    exit();
 }
 
+#[cfg(not(feature = "microram"))]
 fn main() {
     env_logger::init();
-    unsafe { load_concrete_state() };
-    unsafe { load_advice() };
-    run().unwrap();
+    unsafe { emulate_snapshot::load_concrete_state() };
+    unsafe { emulate_advice::load_advice() };
+    run();
 }
 
-
-#[derive(Default)]
-struct AdviceStream {
-    data: Vec<advice::Value>,
-    pos: usize,
-}
-
-impl AdviceStream {
-    pub fn new(data: Vec<advice::Value>) -> AdviceStream {
-        AdviceStream { data, pos: 0 }
-    }
-
-    pub fn next(&mut self) -> advice::Value {
-        assert!(self.pos < self.data.len(),
-            "tried to read past end of advice stream of length {}", self.data.len());
-        let x = self.data[self.pos];
-        self.pos += 1;
-        x
-    }
-}
-
-thread_local! {
-    static ADVICE: RefCell<AdviceStream> = RefCell::new(AdviceStream::default());
-}
-
-unsafe fn load_advice() {
-    // Load advice
-    let f = File::open("advice/linear.cbor").unwrap();
-    let advice: Vec<_> = serde_cbor::from_reader(f).unwrap();
-    ADVICE.with(|c| {
-        *c.borrow_mut() = AdviceStream::new(advice);
-    });
-}
-
-#[no_mangle]
-extern "C" fn __cc_advise(max: advice::Value) -> advice::Value {
-    let x = ADVICE.with(|c| c.borrow_mut().next());
-    assert!(x <= max);
-    x
+#[cfg(feature = "microram")]
+fn main() {
+    run();
 }
