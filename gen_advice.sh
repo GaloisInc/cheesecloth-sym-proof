e3d7690d8f8ed04ad6dd78d1ffe2337592ed8284
Stuart Pernsteiner <spernsteiner@galois.com>
2024-01-29 15:38:08 -0800

    gen_advice.sh: build in release mode

--- gen_advice.sh
+++ gen_advice.sh
@@ -28,8 +28,8 @@ edo() {
 cargo_flags=--release
 
 rm -f advice/*.cbor
-edo cargo run --bin proof_"$name" --features stage0,verbose,debug_symbolic -- "$trace" |& tee stage0.log
-edo cargo run --bin interp_"$name" --features stage1,verbose -- "$trace" |& tee stage1.log
-edo cargo run --bin interp_"$name" --features stage2,verbose -- "$trace" |& tee stage2.log
-edo cargo run --bin interp_"$name" --features stage3,verbose -- "$trace" |& tee stage3.log
-edo cargo run --bin interp_"$name" --features stage4,verbose -- "$trace" |& tee stage4.log
+edo cargo run $cargo_flags --bin proof_"$name" --features stage0,verbose,debug_symbolic -- "$trace" |& tee stage0.log
+edo cargo run $cargo_flags --bin interp_"$name" --features stage1,verbose -- "$trace" |& tee stage1.log
+edo cargo run $cargo_flags --bin interp_"$name" --features stage2,verbose -- "$trace" |& tee stage2.log
+edo cargo run $cargo_flags --bin interp_"$name" --features stage3,verbose -- "$trace" |& tee stage3.log
+edo cargo run $cargo_flags --bin interp_"$name" --features stage4,verbose -- "$trace" |& tee stage4.log
